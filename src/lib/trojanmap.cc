274499c57b9f83b78156dd6c286d9f78621c1a9d
Longhao Yang <longhaoy@usc.edu>
2022-04-05 15:44:14 -0700

    Phase 1 Longhao Y

--- src/lib/trojanmap.cc
+++ src/lib/trojanmap.cc
@@ -112,15 +112,15 @@ int TrojanMap::CalculateEditDistance(std::string a, std::string b){
   int n = b.length();
 
   // Tabluation
-  int dp[m][n];
+  int dp[m+1][n+1];
   for (int i = 0; i <= m; i++) {
-    for (int j = 0; j < n; j++) {
+    for (int j = 0; j <= n; j++) {
       if (i == 0) dp[i][j] = j;
       else if (j == 0) dp[i][j] = i;
       else {
         if (a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1];
         else {
-          dp[i][j] = std::min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+          dp[i][j] = std::min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
         }
       }
     }
